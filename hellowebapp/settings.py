7f7718fa5cc582db33b1ce42c78108e1c4a4de5a
Marouane-Blej <blejmarouane@gmail.com>
2018-03-18 17:53:21 +0100

    Still in progress...

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'collection', #app should be added here
+    'collection',#app should be added here
+    'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
