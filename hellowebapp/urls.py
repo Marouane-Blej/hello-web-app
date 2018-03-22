ed33b2eee32bedccd986b4d33ffeca831018d367
Marouane-Blej <blejmarouane@gmail.com>
2018-03-22 18:55:39 +0100

    4rd commit. Next we'll add a registration form.

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
 
+''' Old urls.
 urlpatterns = [
     url(r'^$', views.index, name='home'),
     # The new URL entries we're adding:
@@ -13,5 +14,25 @@ urlpatterns = [
         TemplateView.as_view(template_name='contact.html'),
         name='contact'),
 
+    url(r'^admin/', admin.site.urls),
+]
+'''
+
+# the new looks like this
+
+urlpatterns = [
+    url(r'^$', views.index, name='home'),
+    url(r'^about/$',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
+    url(r'^contact/$',
+        TemplateView.as_view(template_name='contact.html'),
+        name='contact'),
+    url(r'^things/(?P<slug>[-\w]+)/$', views.thing_detail,
+        name='thing_detail'),
+    # new line we're adding!
+    url(r'^things/(?P<slug>[-\w]+)/edit/$',
+        views.edit_thing,
+        name='edit_thing'),
     url(r'^admin/', admin.site.urls),
 ]
