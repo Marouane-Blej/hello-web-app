7484d8187da29ebfca39d32145f88ba80a2270e1
Marouane-Blej <blejmarouane@gmail.com>
2018-03-22 18:36:02 +0100

    3rd commit. Still in progress...

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+# import your model
+from collection.models import Thing
 
-# Register your models here.
+# set up automated slug creation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it
+admin.site.register(Thing, ThingAdmin)
--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from collection.models import Thing
 
+''' Playing around
 def index(request):
     number = 6
     # don't forget the quotes because it's a string,
@@ -9,4 +11,13 @@ def index(request):
         'number': number,
         # don't forget to pass it in, and the last comma
         'thing': thing,
+    })
+'''
+
+# The view according to our database
+def index(request):
+    things = Thing.objects.all()
+
+    return render(request, 'index.html', {
+        'things': things,
     })
